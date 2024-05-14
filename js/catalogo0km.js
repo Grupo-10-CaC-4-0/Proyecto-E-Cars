45d28e1971c06ea49d816345749c49ff41beebf5
unknown <adolfoglujan@gmail.com>
2024-05-13 21:56:45 -0300

    sumo filtros dinamicos

--- js/catalogo0km.js
+++ js/catalogo0km.js
@@ -48,7 +48,7 @@ let destacados0km = [
         imagensrc: "/img/catalog_0km/mini-kwid-etech.jpg",
     },
     {
-        marca: "Volskwagen",
+        marca: "Volkswagen",
         modelo: "Amarok",
         segmento: "Pick Up",
         combustible: "Diesel",
@@ -138,7 +138,7 @@ let autos0km = [
         imagensrc: "/img/catalog_0km/mini-kwid-etech.jpg",
     },
     {
-        marca: "Volskwagen",
+        marca: "Volkswagen",
         modelo: "Amarok",
         segmento: "Pick Up",
         combustible: "Diesel",
@@ -237,6 +237,8 @@ var card = document.querySelector(".loop0km");
 
 function loadcards() {
 
+    console.log("se ejecuto onload - eliminar linea 240 caalogo0km js");
+
     cargarfiltros();
 
     for (e of destacados0km) {
@@ -260,45 +262,46 @@ var filtrosegmento = document.querySelector(".segmentofiltro");
 var filtrocombustible = document.querySelector(".combustiblefiltro");
 var filtrotransmision = document.querySelector(".transmisionfiltro");
 
+
 function cargarfiltros() {
 
     let listcar = autos0km;
 
-    filtromarca.innerHTML = "<option value='o'> Marca </option>";
-    filtromodelo.innerHTML = "<option value='o'> Modelo </option>";
-    filtrosegmento.innerHTML = "<option value='o'> Segmento </option>";
-    filtrocombustible.innerHTML = "<option value='o'> Combustible </option>";
-    filtrotransmision.innerHTML = "<option value='o'> Transmisión </option>";
-    
+    filtromarca.innerHTML = "<option value='0'> Marca </option><option value='0'> Todos </option>";
+    filtromodelo.innerHTML = "<option value='0'> Modelo </option><option value='0'> Todos </option>";
+    filtrosegmento.innerHTML = "<option value='0'> Segmento </option><option value='0'> Todos </option>";
+    filtrocombustible.innerHTML = "<option value='0'> Combustible </option><option value='0'> Todos </option>";
+    filtrotransmision.innerHTML = "<option value='0'> Transmisión </option><option value='0'> Todos </option>";
+
 
     let marcastemp = [];
     let modelostemp = [];
     let segmentostemp = [];
     let combustiblestemp = [];
     let transmisionestemp = [];
-    
+
 
     for (e of listcar) {
 
         if (!marcastemp.includes(e.marca)) {
             marcastemp.push(e.marca);
-            filtromarca.innerHTML += "<option value='0'>" + e.marca + "</option>"
+            filtromarca.innerHTML += "<option id='eleccion' value='" + (marcastemp.length) + "'>" + e.marca + "</option>"
         }
         if (!modelostemp.includes(e.modelo)) {
             modelostemp.push(e.modelo);
-            filtromodelo.innerHTML += "<option value='0'>" + e.modelo + "</option>"
+            filtromodelo.innerHTML += "<option value='" + (modelostemp.length) + "'>" + e.modelo + "</option>"
         }
         if (!segmentostemp.includes(e.segmento)) {
             segmentostemp.push(e.segmento);
-            filtrosegmento.innerHTML += "<option value='o'>" + e.segmento + "</option>"
+            filtrosegmento.innerHTML += "<option value='" + (segmentostemp.length) + "'>" + e.segmento + "</option>"
         }
         if (!combustiblestemp.includes(e.combustible)) {
             combustiblestemp.push(e.combustible);
-            filtrocombustible.innerHTML += "<option value='o'>" + e.combustible + "</option>"
+            filtrocombustible.innerHTML += "<option value='" + (combustiblestemp.length) + "'>" + e.combustible + "</option>"
         }
         if (!transmisionestemp.includes(e.transmisión)) {
             transmisionestemp.push(e.transmisión);
-            filtrotransmision.innerHTML += "<option value='o'>" + e.transmisión + "</option>"
+            filtrotransmision.innerHTML += "<option value='" + (transmisionestemp.length) + "'>" + e.transmisión + "</option>"
         }
 
     }
@@ -307,3 +310,116 @@ function cargarfiltros() {
 // -------> fin cargar filtros <-------
 
 
+
+
+// -------> funcion de busqueda por filtros <-------
+
+var listbusqueda = [];
+
+let btnmostrarDestacados = document.getElementById('mostrarDestacados');
+function mostrarDestacados(){
+    cargarfiltros();
+    card.innerHTML ="";
+
+    for (e of destacados0km) {
+        let imagen = new Image();
+        imagen.src = e.imagensrc;
+
+        card.innerHTML += "<div class='card0km'> <div><img src=" + imagen.src + " alt=''></div>" + "<div> <div class='titcard0km'>" + e.marca + " " + e.modelo + "</div> <div><ul> <li class='kmcard'>Kilometraje: " + e.km + "km</li> <li class='combuscard'>Combustible: " + e.combustible + "</li> <li class='transmisioncard'>Transmisión: " + e.transmisión + "</li> </ul></div> </div>" + "</div>";
+
+    }
+
+}
+btnmostrarDestacados.addEventListener('click', mostrarDestacados);
+
+
+let btnmostrarTodos = document.getElementById('mostrarTodos');
+function mostrartodos(){
+    cargarfiltros();
+    card.innerHTML ="";
+
+    for (e of autos0km) {
+        let imagen = new Image();
+        imagen.src = e.imagensrc;
+
+        card.innerHTML += "<div class='card0km'> <div><img src=" + imagen.src + " alt=''></div>" + "<div> <div class='titcard0km'>" + e.marca + " " + e.modelo + "</div> <div><ul> <li class='kmcard'>Kilometraje: " + e.km + "km</li> <li class='combuscard'>Combustible: " + e.combustible + "</li> <li class='transmisioncard'>Transmisión: " + e.transmisión + "</li> </ul></div> </div>" + "</div>";
+
+    }
+
+}
+
+btnmostrarTodos.addEventListener('click', mostrartodos);
+
+
+var marcaeleccion = document.getElementById("marcafiltro");
+var modeloeleccion = document.getElementById("modelofiltro");
+var segmentoeleccion = document.getElementById("segmentofiltro");
+var combustibleeleccion = document.getElementById("combustiblefiltro");
+var transmisioneleccion = document.getElementById("transmisionfiltro");
+
+
+
+marcaeleccion.addEventListener("change", opseleccionada);
+modeloeleccion.addEventListener("change", opseleccionada);
+segmentoeleccion.addEventListener("change", opseleccionada);
+combustibleeleccion.addEventListener("change", opseleccionada);
+transmisioneleccion.addEventListener("change", opseleccionada);
+
+function opseleccionada(){
+    let electOption = this.options[this.selectedIndex];
+    console.log(electOption.text);
+    busqueda(electOption.text);
+}
+
+
+
+
+function busqueda(tagfiltro) {
+    if ("Todos" === tagfiltro){
+        mostrartodos();
+    }
+    for (e of autos0km) {
+
+        if (e.marca == tagfiltro) {
+
+            if (!listbusqueda.includes(e)) {
+                listbusqueda.push(e);
+                cargalistalbuscar();
+            };
+
+        } else if (e.modelo == tagfiltro) {
+            if (!listbusqueda.includes(e)) {
+                listbusqueda.push(e);
+                cargalistalbuscar();
+            }
+        } else if(e.segmento == tagfiltro){
+            if (!listbusqueda.includes(e)) {
+                listbusqueda.push(e);
+                cargalistalbuscar();
+            }
+        }else if(e.combustible == tagfiltro){
+            if (!listbusqueda.includes(e)) {
+                listbusqueda.push(e);
+                cargalistalbuscar();
+            }
+        }else if(e.transmisión == tagfiltro){
+            if (!listbusqueda.includes(e)) {
+                listbusqueda.push(e);
+                cargalistalbuscar();
+            }
+        }
+    }
+    listbusqueda.splice(0);
+}
+
+var card2 = document.querySelector("#loop0km");
+function cargalistalbuscar() {
+    card2.innerHTML = "";
+    for (e of listbusqueda) {
+        let imagen = new Image();
+        imagen.src = e.imagensrc;
+
+        card2.innerHTML += "<div class='card0km'> <div><img src=" + imagen.src + " alt=''></div>" + "<div> <div class='titcard0km'>" + e.marca + " " + e.modelo + "</div> <div><ul> <li class='kmcard'>Kilometraje: " + e.km + "km</li> <li class='combuscard'>Combustible: " + e.combustible + "</li> <li class='transmisioncard'>Transmisión: " + e.transmisión + "</li> </ul></div> </div>" + "</div>";
+
+    }
+}
